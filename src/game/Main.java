2a8b699f9bbd90f01b62770d4e6b33d9ccac5762
vasy <https://github.com/solomon202/NewsApp>
2024-05-26 23:18:46 +0300

    three

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,12 +2,138 @@ package game;
 
 import java.awt.Dimension;
 import display.Display;
+import java.awt.Graphics2D;
 
-public class Main {
+import com.thebyteguru.IO.Input;
+import display.Display;
+import com.thebyteguru.graphics.TextureAtlas;
+import utils.Time;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Display.create(800,600,"Tank");
+public class Main implements Runnable{
+	public static final int		WIDTH			= 800;
+	public static final int		HEIGHT			= 600;
+	public static final String	TITLE			= "Tanks";
+	public static final int		CLEAR_COLOR		= 0xff000000;
+	public static final int		NUM_BUFFERS		= 3;
+
+	public static final float	UPDATE_RATE		= 60.0f;
+	public static final float	UPDATE_INTERVAL	= Time.SECOND / UPDATE_RATE;
+	public static final long	IDLE_TIME		= 1;
+
+	public static final String	ATLAS_FILE_NAME	= "texture_atlas.png";
+
+	private boolean				running;
+	private Thread				gameThread;
+	private Graphics2D			graphics;
+	private Input				input;
+	private TextureAtlas		atlas;
+	private Player				player;
+
+	public Game() {
+		running = false;
+		Display.create(WIDTH, HEIGHT, TITLE, CLEAR_COLOR, NUM_BUFFERS);
+		graphics = Display.getGraphics();
+		input = new Input();
+		Display.addInputListener(input);
+		atlas = new TextureAtlas(ATLAS_FILE_NAME);
+		player = new Player(300, 300, 2, 3, atlas);
 	}
 
+	public synchronized void start() {
+
+		if (running)
+			return;
+
+		running = true;
+		//Объекты, реализующие этот интерфейс, могут выполняться потоком
+		gameThread = new Thread(this);
+		gameThread.start();
+
+	}
+
+	public synchronized void stop() {
+
+		if (!running)
+			return;
+
+		running = false;
+
+		try {
+			gameThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		cleanUp();
+
+	}
+
+	private void update() {
+		player.update(input);
+	}
+//выводит кадр 
+	private void render() {
+		Display.clear();
+		player.render(graphics);
+		Display.swapBuffers();
+	}
+	//Объекты, реализующие этот интерфейс, могут выполняться потоком
+//реализуем  метод интерфейса 
+	public void run() {
+
+		int fps = 0;
+		int upd = 0;
+		int updl = 0;
+
+		long count = 0;
+
+		float delta = 0;
+
+		long lastTime = Time.get();
+		while (running) {
+			long now = Time.get();
+			long elapsedTime = now - lastTime;
+			lastTime = now;
+
+			count += elapsedTime;
+
+			boolean render = false;
+			delta += (elapsedTime / UPDATE_INTERVAL);
+			while (delta > 1) {
+				update();
+				upd++;
+				delta--;
+				if (render) {
+					updl++;
+				} else {
+					render = true;
+				}
+			}
+
+			if (render) {
+				render();
+				fps++;
+			} else {
+				try {
+					Thread.sleep(IDLE_TIME);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (count >= Time.SECOND) {
+				Display.setTitle(TITLE + " || Fps: " + fps + " | Upd: " + upd + " | Updl: " + updl);
+				upd = 0;
+				fps = 0;
+				updl = 0;
+				count = 0;
+			}
+
+		}
+
+	}
+
+	private void cleanUp() {
+		Display.destroy();
+}
 }
