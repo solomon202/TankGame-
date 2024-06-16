1ceec0b45fb83c707dd566cc258a5b91867438cc
vasy <https://github.com/solomon202/NewsApp>
2024-06-16 12:55:05 +0300

    seven

--- src/display/Display.java
+++ src/display/Display.java
@@ -9,6 +9,7 @@ import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
 import java.awt.RenderingHints;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.Arrays;
@@ -21,91 +22,82 @@ import IO.Input;
 // Абстрактный класс похож на обычный класс. В абстрактном классе также можно определить поля и
 //методы, но в то же время нельзя создать объект или экземпляр абстрактного класса
 //класс экран обстрактный потому что единственный экран 
-public  abstract class Display {
-	//проверка окно создано или нет 
-	private static boolean			created	= false;
-	// рамка
-	private static JFrame			window;
-	//чистый лист 
-	private static Canvas			content;
-	//класс который содержит изображение чтобы ресовалась вся отдельная картинка а не по очереди компоненты 
-	private static BufferedImage	buffer;
-	// хранит лист байтов изображения 
-	private static int[]			bufferData;
-	private static Graphics			bufferGraphics;
-	//цвет для отчистки листа 
-	private static int				clearColor;
- 
-	//создать рамку  статический метод .размер  окна имя 
-	public static void create(int width, int height, String title) {
-      //если окно есть то все норм 
-		if (created)
-			return;
-		
-		//создания рамки и чистого листа 
-		window = new JFrame(title);
-		//закрыть программу при нажатии на крестик 
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		content = new Canvas();
-		//размер листа  Это что то типа константы, которую ты можешь назначить своему приложению и использовать для всех окон, 
-		//и если тебе захочется все размеры всех форм в приложении изменить, просто меняешь в одном месте. 
-		Dimension size = new Dimension(width, height);
-		//метод листа размер 
-		content.setPreferredSize(size);
-		
-		//методы рамки 
-		
-		//постоянный размер окна чтобы нельзя менять размер окна 
-		window.setResizable(false);
-		//добовляем нашь лист на рамку 
-		window.getContentPane().add(content);
-		//изменет размер окна точьно под нашь лист что бы небыло наслоений
-		window.pack();
-		//позицыя она на экране 
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);
-		
-		//наша картина в буфере всей графики выводится на лист
-		buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);//статическое поле BufferedImage.TYPE_INT_ARGB
-		//информация о нашей графике вытащить  и в ставить в нашу перемкнную лист байтов изображения 
-		//вызвали  класс DataBufferInt расширения его датабуфер 
-		bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
-		//буфер графики туда передали наши картинки 
-		bufferGraphics = buffer.getGraphics();
-		//функция сглаживания 
-		((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-				RenderingHints.VALUE_ANTIALIAS_ON);
-		clearColor = clearColor;
-		//создалась дошла до этого метода 
-		created = true;
-}
-	//очищяет лист 
-	public static void clear() {
-		Arrays.fill(bufferData, clearColor);
 
-	}
-  //возвращает графический обьект  для изменения с наружи нашей графики 
-	public static Graphics2D getGraphics() {
-		return (Graphics2D) bufferGraphics;
-	}
-//уничтожить окно 
-	public static void destroy() {
+public abstract class Display {
 
-		if (!created)
-			return;
 
-		window.dispose();
+private static boolean			created	= false;
+private static JFrame			window;
+private static Canvas			content;
 
-	}
+private static BufferedImage	buffer;
+private static int[]			bufferData;
+private static Graphics			bufferGraphics;
+private static int				clearColor;
 
-	public static void setTitle(String title) {
+private static BufferStrategy	bufferStrategy;
 
-		window.setTitle(title);
+public static void create(int width, int height, String title, int _clearColor, int numBuffers) {
 
-	}
+	if (created)
+		return;
 
-	public static void addInputListener(Input inputListener) {
-		window.add(inputListener);
-	}
+	window = new JFrame(title);
+	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	content = new Canvas();
+
+	Dimension size = new Dimension(width, height);
+	content.setPreferredSize(size);
+
+	window.setResizable(false);
+	window.getContentPane().add(content);
+	window.pack();
+	window.setLocationRelativeTo(null);
+	window.setVisible(true);
+
+	buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+	bufferGraphics = buffer.getGraphics();
+	((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	clearColor = _clearColor;
+
+	content.createBufferStrategy(numBuffers);
+	bufferStrategy = content.getBufferStrategy();
+
+	created = true;
+
 }
 
+public static void clear() {
+	Arrays.fill(bufferData, clearColor);
+}
+
+public static void swapBuffers() {
+	Graphics g = bufferStrategy.getDrawGraphics();
+	g.drawImage(buffer, 0, 0, null);
+	bufferStrategy.show();
+}
+
+public static Graphics2D getGraphics() {
+	return (Graphics2D) bufferGraphics;
+}
+
+public static void destroy() {
+
+	if (!created)
+		return;
+
+	window.dispose();
+
+}
+
+public static void setTitle(String title) {
+
+	window.setTitle(title);
+
+}
+
+public static void addInputListener(Input inputListener) {
+	window.add(inputListener);
+}
+}
